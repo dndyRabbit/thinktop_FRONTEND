d7852ef7fa0be5fa3c20b4251e5463110fa15293
dndyRabbit <dendybgt@gmail.com>
2022-09-23 17:30:06 +0700

    delete useless file

--- src/core/redux/reducers/index.js
+++ src/core/redux/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from "redux";
 
 import akun from "./akun.reducer";
 import jurnal from "./jurnal.reducer";
-import bukuBesar from "./bukuBesar.reducer";
 
 export default combineReducers({
   akun,
   jurnal,
-  bukuBesar,
 });
