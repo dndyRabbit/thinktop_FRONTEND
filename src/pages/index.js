0d806bfb37d291f69c5f395ae4ac3d7f3ae81aca
dndyRabbit <dendybgt@gmail.com>
2022-10-04 16:32:29 +0700

    remove beranda from stack of array

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const Beranda = () => {
             spacing={{ xs: 2, md: 3 }}
             columns={{ xs: 4, sm: 8, md: 12 }}
           >
-            {Menu?.map((item, index) => (
+            {Menu?.slice(1).map((item, index) => (
               <Grid item xs={2} sm={4} md={4} key={index}>
                 <LoadingButton
                   sx={{ width: "100%" }}
