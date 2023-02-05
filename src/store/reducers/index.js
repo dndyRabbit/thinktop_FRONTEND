0ecb1e4ff9a3274c4d00fa7564a468ae206046ec
telkom.yanuardi <yanuardi48@gmail.com>
2023-02-05 21:03:30 +0700

    grapik

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,10 +3,12 @@ import auth from "./authReducer";
 import produk from "./produkReducer";
 import akun from "./akunReducer";
 import pembelian from "./pembelianReducer";
+import dashboard from "./dashboard";
 
 export default combineReducers({
   auth,
   produk,
   akun,
-  pembelian
+  pembelian,
+  dashboard
 });
